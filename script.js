74af06882132f841c260c46587a429883e0a900f
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-10-03 17:22:00 +0330

    added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const menu = document.getElementById('menu');
 const cross = document.getElementById('cross');
 const list = document.getElementById('list');
+const arrow = document.getElementById('arrow');
+const arrowUp = document.getElementById('arrow-2');
+const listMain = document.getElementById('list-2');
+
 menu.addEventListener('click', () => {
     cross.style.display = "block";
     list.style.display = "block";
@@ -10,4 +14,14 @@ cross.addEventListener('click', () => {
     cross.style.display = "none";
     list.style.display = "none";
     menu.style.display = "block";
+})
+arrow.addEventListener('click', () => {
+    arrow.style.display = "none";
+    listMain.style.display = "block";
+    arrowUp.style.display = "block";
+})
+arrowUp.addEventListener('click', () => {
+    arrowUp.style.display = "none";
+    listMain.style.display = "none";
+    arrow.style.display = "block";
 })
